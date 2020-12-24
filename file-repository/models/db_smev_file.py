fb44b2101b5f73854d76b741d81b48187f41eae4
gosha20777 <gosha20777@live.ru>
2020-12-24 15:40:09 +0300

    fix: pg-shema

--- file-repository/models/db_smev_file.py
+++ file-repository/models/db_smev_file.py
@@ -1,9 +1,12 @@
 from sqlalchemy import Boolean, Column, Integer, String, Unicode, DateTime
 from models.db import Base
 from datetime import datetime
+import os
+schema = os.environ['POSTGRES_SCHEMA'] # smev3_client
 
 class SmevFile(Base):
     __tablename__ = "files"
-    __table_args__ = {'schema': 'smev3_client'}
+    if schema != 'NULL':
+        __table_args__ = {'schema': schema}
     id = Column(String, primary_key=True, index=True)
     path = Column(String, unique=True)
--- record-repository/models/db_record.py
+++ record-repository/models/db_record.py
@@ -2,10 +2,14 @@ from sqlalchemy import Boolean, Column, Integer, String, Unicode, DateTime, Fore
 from models.db import Base
 from datetime import datetime
 from sqlalchemy.orm import relationship
+import os
+schema = os.environ['POSTGRES_SCHEMA']
 
 class Record(Base):
     __tablename__ = "smev_records"
-    __table_args__ = {'schema': 'smev3_client'}
+
+    if schema != 'NULL':
+        __table_args__ = {'schema': schema }
     id = Column(String, primary_key=True, index=True)
     date = Column(DateTime, default=datetime.now())
     get_request_request = Column(String)
@@ -17,12 +21,20 @@ class Record(Base):
     send_response_request = Column(String)
     send_response_response = Column(String)
     ack_request = Column(String)
-    attachments = relationship('Attachment', backref='smev3_client.smev_records',
+    if schema != 'NULL':
+        attachments = relationship('Attachment', backref=f'{schema}.smev_records',
+                                lazy='dynamic')
+    elif:
+        attachments = relationship('Attachment', backref='smev_records',
                                 lazy='dynamic')
 
 class Attachment(Base):
     __tablename__ = "smev_attachments"
-    __table_args__ = {'schema': 'smev3_client'}
+    if schema != 'NULL':
+        __table_args__ = {'schema': schema }
     id = Column(String, primary_key=True, index=True)
     extension = Column(String)
-    record_id = Column(String, ForeignKey('smev3_client.smev_records.id'))
+    if schema != 'NULL':
+        record_id = Column(String, ForeignKey(f'{schema}.smev_records.id'))
+    elif:
+        record_id = Column(String, ForeignKey(f'smev_records.id'))
